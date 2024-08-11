b42d52711312cbdf183e5480714dde1910619a57
John Rosales <john.p@mynomademail.com>
2024-08-11 23:35:11 +0800

    Emoji now working

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { PaperClipIcon, PhotoIcon, FaceSmileIcon, HandThumbUpIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import NewMessageInput from './NewMessageInput';
 import axios from "axios";
+import EmojiPicker from "emoji-picker-react";
+import { Popover, Transition } from "@headlessui/react";
 
 const MessageInput = ({ conversation = null}) => {
     const [newMessage, setNewMessage] = useState("");
@@ -68,9 +70,14 @@ const MessageInput = ({ conversation = null}) => {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
-                <button className="p-1 text-gray-400 hover:text-gray-300">
-                    <FaceSmileIcon className="w-6 h-6" />
-                </button>
+                <Popover className="p-1 text-gray-400 hover:text-gray-300 relative inline-block">
+                    <Popover.Button className="p-1 text-gray-400 hover:text-gray-300">
+                        <FaceSmileIcon className="w-6 h-6" />
+                    </Popover.Button>
+                    <Popover.Panel className="absolute z-10 left-0 bottom-full">
+                        <EmojiPicker theme="dark" onEmojiClick={event => setNewMessage(newMessage + event.emoji)}></EmojiPicker>
+                    </Popover.Panel>
+                </Popover>
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">
