515d312b492c72bf6d220642ac11aa5ada39b294
John Rosales <john.p@mynomademail.com>
2024-08-11 23:50:04 +0800

    Can now send likes

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -50,6 +50,24 @@ const MessageInput = ({ conversation = null}) => {
         });
     }
 
+    const onLikeClick = () => {
+        if(messageSending) {
+            return;
+        }
+
+        const data = {
+            message: "👍",
+        }
+
+        if(conversation.is_user) {
+            data["receiver_id"] = conversation.id;
+        } else if (conversation.is_group) {
+            data["group_id"] = conversation.id;
+        }
+
+        axios.post(route("message.store"), data);
+    }
+
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
@@ -103,7 +121,7 @@ const MessageInput = ({ conversation = null}) => {
                 )}
             </div>
             <div className="order-3 xs:order-3 p-2 flex">
-                <button className="p-1 text-gray-400 hover:text-gray-300">
+                <button onClick={onLikeClick} className="p-1 text-gray-400 hover:text-gray-300">
                     <HandThumbUpIcon className="w-6 h-6" />
                 </button>
             </div>
